7422945e72d1b1d8269511f884ce60c8a5511447
Yesid-Ackerman <deironyesithym21@gmail.com>
2024-09-16 09:04:34 -0500

    Archivos en blanco para vistas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\AddChildController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ProfileFatherController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('iniciar-sesion',[LoginController::class,'store']);
+Route::get('Register',[RegisterController::class,'store']);
+Route::get('Add_Child',[AddChildController::class,'store']);
+Route::get('Profile_Father',[ProfileFatherController::class,'store']);
