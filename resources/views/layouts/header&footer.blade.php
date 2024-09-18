05059267c53f82ebaceadb7ad61a6dde6ce0078e
Yesid-Ackerman <deironyesithym21@gmail.com>
2024-09-18 17:42:43 -0500

    Terminar Footer, Falta cont 2 y cont 3

--- resources/views/layouts/header&footer.blade.php
+++ resources/views/layouts/header&footer.blade.php
@@ -7,6 +7,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400..700&family=Press+Start+2P&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Happy+Monkey&display=swap" rel="stylesheet">
     @vite('resources/css/app.css')
     <title>Document</title>
@@ -27,8 +29,42 @@
     @yield('content')
 
         {{-- Footer --}}
-    <footer  class="h-40 w-full bg-[#B43654] border-black">
-
+    <footer  class="h-56 w-full bg-[#282424] border-black">
+        <div class="flex flex-cols-2 w-full h-4/5">
+            <div class="w-1/2 h-full  flex flex-col items-start justify-center p-8">
+                <!-- Título con línea debajo -->
+                <div class="relative flex items-center">
+                    <h1 class="text-3xl text-transparent relative ml-10" style="font-family: 'Press Start 2P';">
+                        <!-- Texto con gradiente vertical encima -->
+                        <span class="absolute -top-1 -left-1 bg-gradient-to-b from-[#F7CD55] to-[#FCA142] bg-clip-text text-transparent">
+                            WOOPERLAND
+                        </span>
+                        <!-- Sombra 3D debajo -->
+                        <span class="text-[#F2446B]">WOOPERLAND</span>
+                    </h1>
+                    <!-- Línea debajo del texto -->
+                    <div class="absolute bottom-0 left-0 w-full flex justify-center ml-6">
+                        <div class="w-full border border-[#FFFFFF]"></div>
+                    </div>
+                </div>
+            
+                <!-- Iconos de redes sociales -->
+                <div class="flex space-x-6 mt-6 ml-24">
+                    
+                    <a href="" target="_blank" class="text-pink-500 text-5xl hover:text-pink-700">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                    <a href="" target="_blank" class="text-red-600 text-5xl hover:text-red-800 ">
+                        <i class="fab fa-youtube"></i>
+                    </a>
+                    <a href="" target="_blank" class="text-blue-500 text-5xl hover:text-blue-700">
+                        <i class="fab fa-facebook"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="w-1/2 h-full ">Cont 2</div>            
+        </div>
+            <div class="w-full h-1/5 bg-red-500">Con 3</div>
     </footer>
 
 </body>
--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -3,24 +3,42 @@
 {{-- Content --}}
     <main class="h-full   w-full bg-gradient-to-b from-[#FFD166] to-[#F78C6B] " style="font-family: 'Happy Monkey">
         <div class=" h-full flex flex-cols-2 ml-28">
-            <div class="w-2/5 h-full my-12 ml-2 pb-8">
+            <div class="w-2/5 h-[95%] my-12 ml-2 pb-8">
                 <img src="{{asset('img/login_image.png')}}" class="h-[90%] w-full rounded-l-lg ">
             </div>
-            <div class="w-[50%] h-[95%] mt-4 bg-gradient-to-b from-[#EF476F] to-[#892940] rounded-2xl items-center">
-                <h1 class="text-center text-white text-2xl p-4">Iniciar Sesión</h1>
-                <form class="w-[80%] h-[50%] grid items-center">                    
-                    <div class="flex flex-col">
-                        <label class="text-white mb-1 p-2 rounded ml-20">Nombre de Usuario:</label>
-                        <input class="w-full p-2 rounded-2xl mx-20 ">
+            <div class="w-1/2 h-[90%] mt-4 bg-gradient-to-b from-[#EF476F] to-[#892940] rounded-2xl p-8">
+                <h1 class="text-center text-white text-5xl p-4">Iniciar Sesión</h1>
+                <form action="" class="w-full grid gap-8 mt-6">
+                    <div class="flex flex-col w-full">
+                        <label class="text-white text-3xl mb-2">Nombre de Usuario:</label>
+                        <input class="w-full h-14 rounded-2xl shadow-md shadow-black px-4 text-3xl" placeholder="Ingresar Nombre de Usuario...">
                     </div>
-                
-                    <div class="flex flex-col">
-                        <label class="text-white mb-1 p-2 rounded ml-20">Contraseña:</label>
-                        <input class="w-full p-2 rounded-2xl mx-20">
-                    </div>                
-                    <button class="w-[80%] p-2 bg-blue-500 text-white rounded mx-32">Iniciar Sesión</button>
+            
+                    <div class="flex flex-col w-full">
+                        <label class="text-white text-3xl mb-2">Contraseña:</label>
+                        <input class="w-full h-14 rounded-2xl shadow-md shadow-black px-4 text-3xl" placeholder="Ingresar Contraseña">
+                    </div>
+                    
+                    <div class="flex justify-center">
+                        <button class="w-1/3 p-4 bg-[#FFD166] text-black rounded-3xl mt-6 text-2xl border-2 border-black shadow-md shadow-black hover:text-white">
+                            Iniciar Sesión
+                        </button>
+                    </div>
                 </form>
-                
+                <div>
+                    <p class="text-white text-2xl text-center mt-12">¿No tienes cuenta?
+                        <a href="" class="text-[#FFD166] text-2xl border-b-2 border-transparent hover:border-[#FFD166] transition duration-100 ease-in-out">
+                            Regístrate
+                        </a>
+                        
+                    </p>
+                </div>
+                <div class="flex flex-col w-full items-center">
+                    <button class="w-8/12 h-16 bg-white text-black rounded-3xl mt-16 text-2xl border-2 border-black shadow-md shadow-black flex items-center justify-center gap-4">
+                        <i class="fab fa-google text-4xl"></i>
+                        Continuar con Google
+                    </button>
+                </div>                
             </div>
         </div>
     </main>
