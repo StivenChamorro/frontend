f97efe3bd9fd45eacbc3c22e09af913fee64cedc
Yesid-Ackerman <deironyesithym21@gmail.com>
2024-09-30 14:55:08 -0500

    Responsive completed of layouts and add_child view

--- resources/views/add/index.blade.php
+++ resources/views/add/index.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.header&footer')
 @section('content')
 {{-- Content --}}
-<main class="h-full w-full bg-gradient-to-b from-[#FFD166] to-[#F78C6B]" style="font-family: 'Happy Monkey">
+<main class="h-full w-full bg-gradient-to-b from-[#FFD166] to-[#F78C6B]" style="font-family: 'Happy Monkey'">
     <div class="h-full flex flex-col mobile:flex-row tablet:ml-28 justify-center items-center">
-        <div class="w-full mobile:w-3/4 tablet:w-2/5 h-[90%] mt-4 bg-gradient-to-b from-[#EF476F] to-[#892940] rounded-2xl p-6 mobile:p-8">
-            <h1 class="text-center text-white text-2xl mobile:text-4xl tablet:text-5xl p-4">Crear Nuevo Perfil</h1>
-            <form action="" class="w-full grid gap-4 mobile:gap-6 mt-6 px-4 tablet:px-8">
+        <div class="w-full mobile:w-3/4 tablet:w-3/4 h-[90%] bg-gradient-to-b from-[#EF476F] to-[#892940] rounded-2xl mobile:p-8">
+            <h1 class="text-center text-white text-2xl mobile:text-4xl tablet:text-4xl p-2">Crear Nuevo Perfil</h1>
+            <form action="" class="w-full grid mobile:gap-6 px-4 tablet:px-4">
                 <!-- Foto de Perfil con ícono de añadir -->
                 <div class="flex flex-col items-center">
-                    <div class="relative w-20 h-20 mobile:w-28 mobile:h-28 tablet:w-32 tablet:h-32 rounded-full border-4 border-white overflow-hidden mb-2">
+                    <div class="relative w-20 h-20 mobile:w-28 mobile:h-28 tablet:w-28 tablet:h-28 rounded-full border-4 border-white overflow-hidden">
                         <img id="profileImage" src="https://via.placeholder.com/150" alt="Foto de Perfil" class="w-full h-full object-cover">
                         <label for="profilePicture" class="absolute bottom-0 right-0 w-8 h-8 tablet:w-10 tablet:h-10 bg-[#FFD166] text-black rounded-full flex items-center justify-center cursor-pointer border-2 border-black shadow-lg z-10">
                             <i class="fas fa-plus"></i>
@@ -19,22 +19,22 @@
 
                 <!-- Nombre -->
                 <div class="flex flex-col w-full">
-                    <input class="w-full h-10 mobile:h-12 tablet:h-12 rounded-2xl shadow-md shadow-black px-4 text-lg mobile:text-2xl tablet:text-3xl" placeholder="Ingresar Nombre">
+                    <input class="w-full h-10 mobile:h-12 tablet:h-12 rounded-2xl shadow-md shadow-black px-4 text-lg mobile:text-xl tablet:text-xl" placeholder="Ingresar Nombre">
                 </div>
                 
                 <!-- Apellido -->
                 <div class="flex flex-col w-full">
-                    <input class="w-full h-10 mobile:h-12 tablet:h-12 rounded-2xl shadow-md shadow-black px-4 text-lg mobile:text-2xl tablet:text-3xl" placeholder="Ingresar Apellido">
+                    <input class="w-full h-10 mobile:h-12 tablet:h-12 rounded-2xl shadow-md shadow-black px-4 text-lg mobile:text-xl tablet:text-xl" placeholder="Ingresar Apellido">
                 </div>
                 
                 <!-- Edad -->
                 <div class="flex flex-col w-full">
-                    <input type="number" id="ageInput" class="w-full h-10 mobile:h-12 tablet:h-12 rounded-2xl shadow-md shadow-black px-4 text-lg mobile:text-2xl tablet:text-3xl" placeholder="Ingresar Edad" min="0" max="10">
+                    <input type="number" id="ageInput" class="w-full h-10 mobile:h-12 tablet:h-12 rounded-2xl shadow-md shadow-black px-4 text-lg mobile:text-xl tablet:text-xl" placeholder="Ingresar Edad" min="0" max="10">
                 </div>
 
                 <!-- Relación con el niño -->
                 <div class="flex flex-col w-full">
-                    <select class="w-full h-10 mobile:h-12 tablet:h-12 rounded-2xl shadow-md shadow-black px-4 text-lg mobile:text-2xl tablet:text-3xl text-gray-400">
+                    <select class="w-full h-10 mobile:h-12 tablet:h-12 rounded-2xl shadow-md shadow-black px-4 text-lg mobile:text-xl tablet:text-xl text-gray-400">
                         <option value="" disabled selected>Relación con el Niño</option>
                         <option class="text-black">Padre/Madre</option>
                         <option class="text-black">Tutor</option>
@@ -43,9 +43,9 @@
                 </div>
 
                 <!-- Género -->
-                <div class="flex items-center gap-4 tablet:gap-6 mt-4">
-                    <label class="text-white text-lg mobile:text-2xl tablet:text-3xl">Género:</label>
-                    <select class="w-full h-10 mobile:h-12 tablet:h-12 rounded-2xl shadow-md shadow-black px-4 text-lg mobile:text-2xl tablet:text-3xl text-gray-400">
+                <div class="flex items-center gap-4">
+                    <label class="text-white text-lg mobile:text-xl tablet:text-xl">Género:</label>
+                    <select class="w-full h-10 mobile:h-12 tablet:h-12 rounded-2xl shadow-md shadow-black px-4 text-lg mobile:text-xl tablet:text-xl text-gray-400">
                         <option value="" disabled selected>Seleccionar Género</option>
                         <option class="text-black">Masculino</option>
                         <option class="text-black">Femenino</option>
@@ -54,12 +54,12 @@
 
                 <!-- Nombre de Usuario -->
                 <div class="flex flex-col w-full">
-                    <input class="w-full h-10 mobile:h-12 tablet:h-12 rounded-2xl shadow-md shadow-black px-4 text-lg mobile:text-2xl tablet:text-3xl" placeholder="Ingresar Nombre de Usuario">
+                    <input class="w-full h-10 mobile:h-12 tablet:h-12 rounded-2xl shadow-md shadow-black px-4 text-lg mobile:text-xl tablet:text-xl" placeholder="Ingresar Nombre de Usuario">
                 </div>
 
                 <!-- Botón Siguiente -->
                 <div class="flex justify-center">
-                    <button class="w-2/3 mobile:w-1/2 tablet:w-1/3 p-2 tablet:p-4 bg-[#FFD166] text-black rounded-3xl text-lg mobile:text-2xl tablet:text-2xl border-2 border-black shadow-md shadow-black hover:text-white">
+                    <button class="w-2/3 mobile:w-1/2 tablet:w-1/2 p-2 bg-[#FFD166] text-black rounded-3xl text-lg mobile:text-xl tablet:text-xl border-2 border-black shadow-md shadow-black hover:text-white">
                         Siguiente
                     </button>
                 </div>
--- resources/views/layouts/header&footer.blade.php
+++ resources/views/layouts/header&footer.blade.php
@@ -6,88 +6,69 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400..700&family=Press+Start+2P&display=swap" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Happy+Monkey&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     @vite('resources/css/app.css')
     <title>Document</title>
 </head>
 <body>
-        {{-- Header --}}
-        <header class="h-24 w-full bg-[#B43654] flex items-center ">
-            <h1 class="text-5xl text-transparent font-bold relative ml-12" style="font-family: 'Press Start 2P' ">
-                <!-- Texto con gradiente vertical encima -->
-                <span class="absolute -top-1 -left-1 bg-gradient-to-b from-[#F7CD55] to-[#FCA142] bg-clip-text text-transparent">
-                    WOOPERLAND
-                </span>
-                <!-- Sombra 3D debajo -->
-                <span class="text-[#F2446B]">WOOPERLAND</span>
-            </h1>
-        </header>        
+    {{-- Header --}}
+    <header class="h-24 w-full bg-[#B43654] flex items-center justify-center">
+        <h1 class="text-4xl tablet:text-5xl mobile:text-4xl text-transparent font-bold relative" style="font-family: 'Press Start 2P' ">
+            <span class="absolute -top-1 -left-1 bg-gradient-to-b from-[#F7CD55] to-[#FCA142] bg-clip-text text-transparent">
+                WOOPERLAND
+            </span>
+            <span class="text-[#F2446B]">WOOPERLAND</span>
+        </h1>
+    </header>        
+
     {{-- Content --}}
     @yield('content')
 
-        {{-- Footer --}}
-        <footer class="h-56 w-full bg-[#282424] border-black">
-            {{-- Div Arriba --}}
-            <div class="flex flex-cols-2 w-full h-4/5">
-                {{-- Div Izquierda --}}
-                <div class="w-1/2 h-full flex flex-col items-start justify-center p-8 border-b">
-                    <!-- Título con línea debajo -->
-                    <div class="relative flex items-center">
-                        <h1 class="text-3xl text-transparent relative ml-10" style="font-family: 'Press Start 2P';">
-                            <!-- Texto con gradiente vertical encima -->
-                            <span class="absolute -top-1 -left-1 bg-gradient-to-b from-[#F7CD55] to-[#FCA142] bg-clip-text text-transparent">
-                                WOOPERLAND
-                            </span>
-                            <!-- Sombra 3D debajo -->
-                            <span class="text-[#F2446B]">WOOPERLAND</span>
-                        </h1>
-                        <!-- Línea debajo del texto -->
-                        <div class="absolute bottom-0 left-0 w-full flex justify-center ml-6">
-                            <div class="w-full border border-[#FFFFFF]"></div>
-                        </div>
+    {{-- Footer --}}
+    <footer class="h-auto w-full bg-[#282424] border-black py-8">
+        <div class="flex flex-col tablet:flex-row w-full max-w-7xl mx-auto">
+            {{-- Div Izquierda --}}
+            <div class="w-full tablet:w-1/2 flex flex-col items-start justify-center p-8 tablet:border-b" style="font-family: 'Press Start 2P'">
+                <div class="relative flex items-center mb-4">
+                    <h1 class="text-3xl mobile:text-2xl tablet:text-3xl text-transparent relative">
+                        <span class="absolute -top-1 -left-1 bg-gradient-to-b from-[#F7CD55] to-[#FCA142] bg-clip-text text-transparent">
+                            WOOPERLAND
+                        </span>
+                        <span class="text-[#F2446B]">WOOPERLAND</span>
+                    </h1>
+                    <div class="absolute bottom-0 left-0 w-full flex justify-center">
+                        <div class="w-full border border-[#FFFFFF]"></div>
                     </div>
+                </div>
                 
-                    <!-- Iconos de redes sociales -->
-                    <div class="flex space-x-6 mt-6 ml-24">
-                        <a href="" target="_blank" class="text-pink-500 text-5xl hover:text-pink-700">
-                            <i class="fab fa-instagram"></i>
-                        </a>
-                        <a href="" target="_blank" class="text-red-600 text-5xl hover:text-red-800">
-                            <i class="fab fa-youtube"></i>
-                        </a>
-                        <a href="" target="_blank" class="text-blue-500 text-5xl hover:text-blue-700">
-                            <i class="fab fa-facebook"></i>
-                        </a>
-                    </div>
-                </div>
-        
-                {{-- Div Derecha --}}
-                <div class="w-1/2 h-full border-b">    
-                    <div class="w-1/2"></div>        
-                    <!-- Sección Legal -->
-                    <div class="w-[60%] flex flex-col items-start justify-center p-2 ml-auto" style="font-family: Happy Monkey">
-                        <!-- Título "Legal" con línea debajo -->
-                        <h1 class="text-2xl text-white">Legal</h1>
-                        <div class="w-1/6 border-b-2 border-white mb-4"></div>                    
-                        <!-- Enlaces de términos y políticas -->
-                        <a href="#" class="text-white text-lg mb-2 hover:underline">Términos de uso</a>
-                        <a href="#" class="text-white text-lg mb-2 hover:underline">Política de Cookies</a>
-                        <a href="#" class="text-white text-lg mb-2 hover:underline">Política de privacidad</a>
-                    </div>
+                <!-- Iconos de redes sociales -->
+                <div class="flex space-x-4 mobile:space-x-6 mt-6">
+                    <a href="" target="_blank" class="text-pink-500 text-4xl mobile:text-5xl hover:text-pink-700">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                    <a href="" target="_blank" class="text-red-600 text-4xl mobile:text-5xl hover:text-red-800">
+                        <i class="fab fa-youtube"></i>
+                    </a>
+                    <a href="" target="_blank" class="text-blue-500 text-4xl mobile:text-5xl hover:text-blue-700">
+                        <i class="fab fa-facebook"></i>
+                    </a>
                 </div>
             </div>
         
-            {{-- Línea horizontal y texto abajo --}}
-            <div class="relative w-full h-1/5 flex items-center" style="font-family: Happy Monkey">
-                <div class="text-center text-white w-full">
-                    @ 2024 wooperland - Todos los derechos reservados
-                </div>
+            {{-- Div Derecha --}}
+            <div class="w-full tablet:w-1/2 flex flex-col items-start justify-center p-8 mobile:border-b">    
+                <h1 class="text-lg mobile:text-xl tablet:text-2xl text-white">Legal</h1>
+                <div class="w-1/6 border-b-2 border-white mb-4"></div>                    
+                <a href="#" class="text-white text-sm mobile:text-base tablet:text-lg mb-2 hover:underline">Términos de uso</a>
+                <a href="#" class="text-white text-sm mobile:text-base tablet:text-lg mb-2 hover:underline">Política de Cookies</a>
+                <a href="#" class="text-white text-sm mobile:text-base tablet:text-lg mb-2 hover:underline">Política de privacidad</a>
             </div>
-        </footer>
-        
-
+        </div>
+    
+        <div class="text-center text-white w-full mt-4" style="font-family: 'Happy Monkey'">
+            @ 2024 wooperland - Todos los derechos reservados
+        </div>
+    </footer>
 </body>
 </html>
